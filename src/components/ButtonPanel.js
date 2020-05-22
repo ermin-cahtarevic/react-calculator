1795937267109c2794f4a11bcfd640900bc3795a
Ermin Cahtarevic <erminc@windowslive.com>
2020-05-22 17:48:08 +0200

    Add styling to App, Display, Button and ButtonPanel components

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import Button from './Button';
+import '../styles/ButtonPanel.css';
 
-const ButtonPanel = () => (
-  <div>
-    <div className="btn-group">
-      <Button buttonName="AC" />
-      <Button buttonName="+/-" />
-      <Button buttonName="%" />
-      <Button buttonName="÷" />
+const ButtonPanel = () => {
+  const white = "rgb(224, 224, 224)";
+  const orange = "rgb(189, 120, 8)"
+  return (
+    <div className="btn-panel">
+      <div className="btn-group">
+        <Button buttonName="AC" color={white} />
+        <Button buttonName="+/-" color={white} />
+        <Button buttonName="%" color={white} />
+        <Button buttonName="÷" color={orange} />
+      </div>
+      <div className="btn-group">
+        <Button buttonName="7" color={white} />
+        <Button buttonName="8" color={white} />
+        <Button buttonName="9" color={white} />
+        <Button buttonName="×" color={orange} />
+      </div>
+      <div className="btn-group">
+        <Button buttonName="4" color={white} />
+        <Button buttonName="5" color={white} />
+        <Button buttonName="6" color={white} />
+        <Button buttonName="-" color={orange} />
+      </div>
+      <div className="btn-group">
+        <Button buttonName="1" color={white} />
+        <Button buttonName="2" color={white} />
+        <Button buttonName="3" color={white} />
+        <Button buttonName="+" color={orange} />
+      </div>
+      <div className="btn-group group-5">
+        <Button buttonName="0" color={white} wide={true} />
+        <Button buttonName="." color={white} />
+        <Button buttonName="=" color={orange} />
+      </div>
     </div>
-    <div className="btn-group">
-      <Button buttonName="7" />
-      <Button buttonName="8" />
-      <Button buttonName="9" />
-      <Button buttonName="×" />
-    </div>
-    <div className="btn-group">
-      <Button buttonName="4" />
-      <Button buttonName="5" />
-      <Button buttonName="6" />
-      <Button buttonName="-" />
-    </div>
-    <div className="btn-group">
-      <Button buttonName="1" />
-      <Button buttonName="2" />
-      <Button buttonName="3" />
-      <Button buttonName="+" />
-    </div>
-    <div className="btn-group">
-      <Button buttonName="0" />
-      <Button buttonName="." />
-      <Button buttonName="=" />
-    </div>
-  </div>
-);
+  );
+}
 
 export default ButtonPanel;
