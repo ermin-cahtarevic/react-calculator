e1bde695b18d9094455780b161ce90d43d184116
Ermin Cahtarevic <erminc@windowslive.com>
2020-05-26 16:32:11 +0200

    Update ButtonPanel with two loops for creating the elements

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,37 +5,33 @@ import '../styles/ButtonPanel.css';
 const ButtonPanel = () => {
   const white = 'rgb(224, 224, 224)';
   const orange = 'rgb(189, 120, 8)';
+
+  const groups = [
+    ['AC', '+/-', '%', '÷'],
+    ['7', '8', '9', '×'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
+  ];
+
   return (
     <div className="btn-panel">
-      <div className="btn-group">
-        <Button buttonName="AC" color={white} />
-        <Button buttonName="+/-" color={white} />
-        <Button buttonName="%" color={white} />
-        <Button buttonName="÷" color={orange} />
-      </div>
-      <div className="btn-group">
-        <Button buttonName="7" color={white} />
-        <Button buttonName="8" color={white} />
-        <Button buttonName="9" color={white} />
-        <Button buttonName="×" color={orange} />
-      </div>
-      <div className="btn-group">
-        <Button buttonName="4" color={white} />
-        <Button buttonName="5" color={white} />
-        <Button buttonName="6" color={white} />
-        <Button buttonName="-" color={orange} />
-      </div>
-      <div className="btn-group">
-        <Button buttonName="1" color={white} />
-        <Button buttonName="2" color={white} />
-        <Button buttonName="3" color={white} />
-        <Button buttonName="+" color={orange} />
-      </div>
-      <div className="btn-group group-5">
-        <Button buttonName="0" color={white} wide />
-        <Button buttonName="." color={white} />
-        <Button buttonName="=" color={orange} />
-      </div>
+      {
+        groups.map((group, i) => (
+          <div key={group} className={`btn-group group-${i + 1}`}>
+            {
+              group.map(sym => (
+                <Button
+                  key={sym}
+                  buttonName={sym}
+                  color={group.indexOf(sym) === group.length - 1 ? orange : white}
+                  wide={sym === '0'}
+                />
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 };
